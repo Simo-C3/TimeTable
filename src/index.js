5b07ad350cc7d57d87075f442c74753a06a6f2cc
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-02-28 14:29:02 +0900

    [add] add main.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Main from './main';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Main/>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-unti
+import React from 'react';
+import Header from './Components/header';
+import Menu from './Components/menu';
+
+class Main extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user_id: "",
+            user_name: "",
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Menu/>
+            </div>
+        )
+    }
+}
+
+export default Main;
